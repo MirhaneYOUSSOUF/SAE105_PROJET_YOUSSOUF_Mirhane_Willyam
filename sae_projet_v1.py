32483e1b51b9f6e8e8d1bf50a13a7037e0c67401
MirhaneYOUSSOUF <mirhane.youssouf@iut-dijon.u-bourgogne.fr>
2026-01-05 12:59:49 +0100

    Rename consumption lists for clarity

--- sae_projet_v1.py
+++ sae_projet_v1.py
@@ -21,58 +21,59 @@ for element in Donnees:
 
 """
 """
+#Donnees de consommation corrigee en semaines
 
-semaine_2025=[]
+semaine_conso_2025=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2025':
         #print(element[0], element[1], element[2], element[6], element[4])
         #print(element[0:])
-        semaine_2025.append(float(element[6].replace(",", ".")))
-#print(semaine_2025)
+        semaine_conso_2025.append(float(element[6].replace(",", ".")))
+#print(semaine_conso_2025)
 
 """
 
 
 
-semaine_2024=[]
+semaine_conso_2024=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2024':
         #print(element[0], element[1], element[2], element[6], element[4])
-        semaine_2024.append(float(element[6].replace(",", ".")))
+        semaine_conso_2024.append(float(element[6].replace(",", ".")))
         #print(element[0:])
-print(semaine_2024)
+print(semaine_conso_2024)
 
 """
 
-semaine_2023=[]
+semaine_conso_2023=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2023':
         #print(element[0:])
-        semaine_2023.append(float(element[6].replace(",", ".")))
-#print(semaine_2023)
+        semaine_conso_2023.append(float(element[6].replace(",", ".")))
+#print(semaine_conso_2023)
 
 
-semaine_2022=[]
+semaine_conso_2022=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2022':
         #print(element[0:])
-        semaine_2022.append(float(element[6].replace(",", ".")))
-print(semaine_2022)
+        semaine_conso_2022.append(float(element[6].replace(",", ".")))
+print(semaine_conso_2022)
 
 
-semaine_2021=[]
+semaine_conso_2021=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2021':
         #print(element[0:])
-        semaine_2021.append(float(element[6].replace(",", ".")))
-#print(semaine_2021)
+        semaine_conso_2021.append(float(element[6].replace(",", ".")))
+#print(semaine_conso_2021)
 
-semaine_2020=[]
+semaine_conso_2020=[]
 for element in Donnees:
     if element[0] == 'semaine' and element[4] == '2020':
         #print(element[0:])
-        semaine_2020.append(float(element[6].replace(",", ".")))
-#print(semaine_2020)
+        semaine_conso_2020.append(float(element[6].replace(",", ".")))
+#print(semaine_conso_2020)
 
 
 

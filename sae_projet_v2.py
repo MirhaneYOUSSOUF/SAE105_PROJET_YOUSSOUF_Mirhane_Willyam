af18f1a9ecf9e4a9bfc63a64a342b4e3bf6643f2
Youssoufwillyam <willyam.youssouf@iut-dijon.u-bourgogne.fr>
2026-01-06 18:04:20 +0100

    Update sae_projet_v2.py

--- sae_projet_v2.py
+++ sae_projet_v2.py
@@ -81,3 +81,110 @@ for r in resultats:
 #    else:
 #        D[3]=D[3]+1
 #print(D)
+
+import csv
+import matplotlib.pyplot as plt
+
+Donnees=[]
+with open('_C3_89volution_de_la_consommation_hebdomadaire_2025-12-18_13-41.csv',newline='') as csvfile:
+    reader=csv.reader(csvfile,delimiter=';')
+    for row in reader:
+        print(','.join(row))
+        Donnees.append(row)
+print(Donnees)
+
+Donnees1=[]
+with open('_C3_89volution_de_la_consommation_hebdomadaire_2025-12-18_13-41.csv',newline='') as csvfile:
+    reader=csv.reader(csvfile,delimiter=',')
+    for row in reader:
+        print(','.join(row))
+        Donnees1.append(row)
+print(Donnees1)
+print(Donnees1[1][4])
+
+
+del Donnees1[0]
+print(Donnees1)
+
+#Donnees de consommation corrigee en jours pour chaque année (2025, 2024, 2023, 2022, 2021, 2020)
+
+jour_2025=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2025' and element[6] != 'NA':
+        jour_2025.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2025.append(float(element[i]))      
+print(jour_2025)
+
+jour_2024=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2024' and element[6] != 'NA':
+        jour_2024.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2024.append(float(element[i]))      
+print(jour_2024)
+
+jour_2023=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2023' and element[6] != 'NA':
+        jour_2023.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2023.append(float(element[i]))      
+print(jour_2023)
+
+jour_2022=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2022' and element[6] != 'NA':
+        jour_2022.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2022.append(float(element[i]))      
+print(jour_2022)
+
+jour_2021=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2021' and element[6] != 'NA':
+        jour_2021.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2021.append(float(element[i]))      
+print(jour_2021)
+
+jour_2020=[]
+for element in Donnees1:
+    if element[0] == 'jour' and element[4] == '2020' and element[6] != 'NA':
+        jour_2020.append(float(element[6].replace(',', '.')))  #Convertir en float
+
+for element in Donnees:
+    for i in range(6, len(element), 2):
+        if element[i] != 'NA' and ligne[i] != '':
+            jour_2020.append(float(element[i]))      
+print(jour_2020)
+
+plt.plot(range(1, len(jour_2025)+1), jour_2025, marker='o', label='2025')
+
+plt.plot(range(1, len(jour_2024)+1), jour_2024, marker='o', label='2024')
+plt.plot(range(1, len(jour_2023)+1), jour_2023, marker='o', label='2023')
+plt.plot(range(1, len(jour_2022)+1), jour_2022, marker='o', label='2022')
+plt.plot(range(1, len(jour_2021)+1), jour_2021, marker='o', label="2021")
+plt.plot(range(1, len(jour_2020)+1), jour_2020, marker='o', label='2020')
+
+plt.xlabel("Jours de l'année")
+plt.ylabel("Consommation corrigée")
+plt.title("Évolution hebdomadaire de la consommation (2020 - 2025)")
+plt.grid(True)
+plt.legend()
+plt.show()
